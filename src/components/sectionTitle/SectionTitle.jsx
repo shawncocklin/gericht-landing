f9875206a2efcc2c4db4e3da236b8bbd00636d72
shawncocklin <shawncocklin@gmail.com>
2022-12-05 20:44:21 -0500

    finish hero with responsive layout

--- src/components/sectionTitle/SectionTitle.jsx
+++ src/components/sectionTitle/SectionTitle.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './sectionTitle.css'
 
-export default function SectionTitle() {
+import { images } from '../../constants'
+
+export default function SectionTitle({ hero, eyebrow, title }) {
   return (
-    <div>
-      <h1>SectionTitle</h1>
+    <div className='section-title'>
+      <p className='eyebrow p__cormorant'>{eyebrow}</p>
+      <img
+        className='spoon__img'
+        src={images.spoon}
+        alt=''
+      />
+      <div className='section-title'>
+        {hero ? (
+          <h1>{title}</h1>
+        ) : (
+          <h2 className='headtext__cormorant'>{title}</h2>
+        )}
+      </div>
     </div>
   )
 }
--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './hero.css'
 
+import { SectionTitle } from '../../components'
+import { images } from '../../constants'
+
 export default function Hero() {
   return (
-    <div>
-      <h1>hero</h1>
+    <div className='hero section__padding'>
+      <div className='hero-content'>
+        <SectionTitle
+          eyebrow='Chase the new Flavour'
+          title='The key to Fine dining'
+          hero={true}
+        />
+        <p className='hero-content_body'>
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus
+        </p>
+        <button>Explore Menu</button>
+      </div>
+      <div className='hero-img'>
+        <img
+          src={images.welcome}
+          alt='a salmon dish covered in an orange sauce with herbs and lemon wedges'
+        />
+      </div>
     </div>
   )
 }
